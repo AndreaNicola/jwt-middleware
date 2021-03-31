04b42f0e0eb5d7a31cf4f553050abcddbe37bb1e
Andrea Nicola <cico.85@gmail.com>
2021-03-31 13:48:23 +0200

    jwt token extraction

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,50 @@
 package jwtmiddleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+	"strings"
+)
+
+var secret []byte
+
+func init() {
+
+}
 
 func JwtMiddleware() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		context.Next()
 	}
+}
+
+func RoleBasedJwtMiddleware(role []string) gin.HandlerFunc {
+	return func(context *gin.Context) {
+
+		jwtTokenString := extractToken(context)
+		log.Info(jwtTokenString)
+
+		context.Next()
+	}
+}
+
+func extractToken(context *gin.Context) string {
+	bearerToken := context.GetHeader("Authorization")
+
+	if bearerToken == "" {
+		panic("no bearer token")
+	}
+
+	strArr := strings.Split(bearerToken, " ")
+
+	if len(strArr) != 2 {
+		panic("no bearer token")
+	}
+
+	return strArr[1]
+
+}
+
+func verifyToken() {
+
 }
